4f4185d8fdc016d6f893b7c1d9ec3bcd7a98f2e4
Jerry Chen <jc2939@rit.edu>
2023-03-04 18:50:26 -0500

    added persistence MilkFileDAO for unit testing

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/MilkFileDAOTest.java
@@ -0,0 +1,64 @@
+package com.estore.api.estoreapi.persistence;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.estore.api.estoreapi.model.Milk;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+@Tag("Persistence-tier")
+public class MilkFileDAOTest {
+    MilkFileDAO milkFileDAO;
+    Milk[] testMilks;
+    ObjectMapper mockObjectMapper;
+
+    /**
+     * Create and inject a Mock Object Mapper to isolate the tests
+     * from the underlying file
+     * @throws IOException
+     */
+    @BeforeEach
+    public void setupMilkFileDAO() throws IOException
+    {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testMilks = new Milk[4];
+        testMilks[0] = new Milk(10, "cow", "banana", 2.5, 8, 2.95);
+        testMilks[1] = new Milk(11, "oat", "chocolate", 4.2, 5, 4.50);
+        testMilks[2] = new Milk(12, "goat", "strawberry", 3.9, 11, 4.23);
+        testMilks[3] = new Milk(13, "almond", "pumpkin", 5.7, 14, 6.03);
+
+        // When the object mapper is supposed to read from the file
+        // the mock object mapper will return the hero array above
+        when(mockObjectMapper.readValue(new File("random.txt"), Milk[].class))
+            .thenReturn(testMilks);
+        milkFileDAO = new MilkFileDAO("random.txt", mockObjectMapper);
+    }
+
+    @Test
+    public void testGetMilks()
+    {
+        Milk[] milks = milkFileDAO.getMilks();
+        // Analyze
+        assertEquals(milks.length, testMilks.length);
+        for (int i = 0; i < testMilks.length; i++)
+        {
+            assertEquals(milks[i], testMilks[i]);
+        }
+    }
+
+}
