3a382869a70231cba144e787aff5ddb99b367549
Daniel Jara <daj4240@rit.edu>
2023-03-18 16:48:44 -0400

    Button added. Increments items in shoppingCart
    Added the Add To Cart button. Increments the count of milk by 1, and adds if not already present

--- estore-ui/angular-milk/src/app/dashboard/dashboard.component.ts
+++ estore-ui/angular-milk/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Milk } from '../milk';
 import { MilkService } from '../milk.service';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../shoppingCart';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +11,18 @@ import { MilkService } from '../milk.service';
 })
 export class DashboardComponent implements OnInit {
   milks: Milk[] = [];
+  shoppingCart: ShoppingCart | undefined;
 
-  constructor(private MilkService: MilkService) { }
+  constructor(private MilkService: MilkService, private ShoppingCartService: ShoppingCartService) { }
 
   ngOnInit(): void {
     this.getMilks();
+    this.getShoppingCart();
+  }
+
+  getShoppingCart(): void {
+    this.ShoppingCartService.getShoppingCart("Jeremy")
+    .subscribe(shoppingCart => this.shoppingCart = shoppingCart);
   }
 
   getMilks(): void {
@@ -21,5 +30,18 @@ export class DashboardComponent implements OnInit {
       .subscribe(milks => this.milks = milks);
   }
 
+  addOne(milk: Milk, userName: String): void {
+    this.ShoppingCartService.incrementMilk(milk, userName).subscribe(() => {
+      const index = this.shoppingCart?.milksInCart.findIndex(item => item.id === milk.id);
+      if (this.shoppingCart && this.shoppingCart.milksInCartQuantity && index !== undefined && index !== -1) {
+        this.shoppingCart.milksInCartQuantity[index] += 1;
+      }
+    });
+    this.reloadPage();
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
 
 }
